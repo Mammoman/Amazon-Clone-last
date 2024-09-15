6660f93ab4b9aefeb643bcfdff4c454a46e9db43
Mammoman <daniel08seun@gmail.com>
2024-09-15 20:08:12 +0100

    huh

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,28 @@
 export const cart = [];
+export function addToCart (productId) {
+    let matchingITEM;
+
+    cart.forEach((cartItem) =>  {
+     if (productId === cartItem.productId) {
+         matchingITEM = cartItem;
+       }
+    });
+
+  
+    const quantitySelector = document.querySelector
+    (`.js-quantity-selector-${productId}`)
+
+    const quantity = Number(quantitySelector.value);
+
+
+     if (matchingITEM) {
+         matchingITEM.quantity += 1;
+         matchingITEM.quantity +=  quantity
+     } else {
+         cart.push({
+             productId: productId,
+             quantity:1,
+             quantity:  quantity
+          })
+     }
+  };
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-   import {cart} from '../data/cart.js';
+   import {cart, addToCart} from '../data/cart.js';
    import {products} from '../data/products.js';
    
    let productsHTML ='';
@@ -63,50 +63,31 @@ document.querySelector('.js-products-grid')
   .innerHTML = productsHTML;
 
 
+  
+  
+
+  function updateCartQuantity () {
+    let cartQuantity = 0;
+
+    cart.forEach((cartItem) => {
+        cartQuantity += cartItem.quantity;
+    });
+
+    document.querySelector('.js-cart-quantity')
+      .innerHTML = cartQuantity;
+
+    console.log(cart);
+
+  };
+
   document.querySelectorAll('.js-add-to-cart')
     .forEach((button) => {
       button.addEventListener('click', () => {
        const productId = 
        button.dataset.productId;
-
-       let matchingITEM;
-
-       cart.forEach((item) =>  {
-        if (productId === item.productId) {
-            matchingITEM = item;
-          }
-       });
-
-     
-       const quantitySelector = document.querySelector
-       (`.js-quantity-selector-${productId}`)
-
-       const quantity = Number(quantitySelector.value);
-
-
-        if (matchingITEM) {
-            matchingITEM.quantity += 1;
-            matchingITEM.quantity +=  quantity
-        } else {
-            cart.push({
-                productId: productId,
-                quantity:1,
-                quantity:  quantity
-             })
-        }
-
-        let cartQuantity = 0;
-
-        cart.forEach((item) => {
-            cartQuantity += item.quantity;
-        });
-
-        document.querySelector('.js-cart-quantity')
-          .innerHTML = cartQuantity;
-
-        console.log(cart);
-
-       
+        addToCart(productId);
+        updateCartQuantity();
+      
           })
       });
   
