e34d463483f9f68114821c75272ebc6ac7767c3a
Mammoman <daniel08seun@gmail.com>
2024-09-15 15:29:02 +0100

    cart-quantity-update

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -82,6 +82,16 @@ document.querySelector('.js-products-grid')
                 quantity:1
              })
         }
+
+        let cartQuantity = 0;
+
+        cart.forEach((item) => {
+            cartQuantity += item.quantity;
+        });
+
+        document.querySelector('.js-cart-quantity')
+          .innerHTML = cartQuantity;
+
         console.log(cart);
       });
     });
