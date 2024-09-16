ceb59cc521bac3f857fedc913ee0ac9547c896ff
Mammoman <daniel08seun@gmail.com>
2024-09-16 15:24:14 +0100

    radio-fix

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,6 @@
    import {cart, addToCart} from '../data/cart.js';
    import {products} from '../data/products.js';
+   import {converter} from './utils/money.js';
    
    let productsHTML ='';
 
@@ -24,7 +25,7 @@
           </div>
 
           <div class="product-price">
-           $${(product.priceCents /100).toFixed(2)}
+           $${converter(product.priceCents)}
           </div>
 
           <div class="product-quantity-container">
